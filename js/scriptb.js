f222ab13e338bebac3090fe9926d2f9221363b6d
vogana <cctv92@126.com>
2023-08-03 23:12:36 +0800

    clothing

--- js/scriptb.js
+++ js/scriptb.js
@@ -1,25 +1,21 @@
-// 当文档加载完成时执行
 document.addEventListener("DOMContentLoaded", function () {
-    // 获取切换颜色按钮和文档主体元素
     const changeColorBtn = document.getElementById("changeColorBtn");
     const body = document.body;
 
-    // 添加点击事件监听器，切换背景颜色
     changeColorBtn.addEventListener("click", changeBackgroundColor);
 
-    // 切换背景颜色函数
     function changeBackgroundColor() {
-        // 定义一组颜色
         const colors = [
             "#CCE8CF", 
             "#C7EDCC", 
-            "#6E7B6C", 
-            "#E3EDCD" 
-        ];
-        // 随机选择一个颜色
+            "#6E7B6C"
+        ]; // 减少颜色数量
         const randomIndex = Math.floor(Math.random() * colors.length);
         const randomColor = colors[randomIndex];
-        // 设置文档主体的背景颜色
+        
+        // 背景颜色过渡效果
+        body.style.transition = "background-color 0.5s ease-in-out";
+        
         body.style.backgroundColor = randomColor;
     }
 });
