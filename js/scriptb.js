ce71c98404c3ed622623c6bde0564e386e262c14
vogana <cctv92@126.com>
2023-08-03 23:03:13 +0800

    clothing

--- js/scriptb.js
+++ js/scriptb.js
@@ -1,23 +1,25 @@
-// 获取按钮元素
-const changeColorBtn = document.getElementById("changeColorBtn");
+// 当文档加载完成时执行
+document.addEventListener("DOMContentLoaded", function () {
+    // 获取切换颜色按钮和文档主体元素
+    const changeColorBtn = document.getElementById("changeColorBtn");
+    const body = document.body;
 
-// 定义一组适合阅读的背景颜色
-const colors = [
-    "#CCE8CF", 
-    "#C7EDCC", 
-    "#6E7B6C", 
-	"#E3EDCD" 
-];
+    // 添加点击事件监听器，切换背景颜色
+    changeColorBtn.addEventListener("click", changeBackgroundColor);
 
-// 为按钮添加点击事件监听器
-changeColorBtn.addEventListener("click", changeBackgroundColor);
-
-// 点击按钮时切换背景颜色
-function changeBackgroundColor() {
-    // 随机选择颜色数组中的一个索引
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    // 获取随机选择的颜色
-    const randomColor = colors[randomIndex];
-    // 将页面的背景颜色设置为随机颜色
-    document.body.style.backgroundColor = randomColor;
-}
+    // 切换背景颜色函数
+    function changeBackgroundColor() {
+        // 定义一组颜色
+        const colors = [
+            "#CCE8CF", 
+            "#C7EDCC", 
+            "#6E7B6C", 
+            "#E3EDCD" 
+        ];
+        // 随机选择一个颜色
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        const randomColor = colors[randomIndex];
+        // 设置文档主体的背景颜色
+        body.style.backgroundColor = randomColor;
+    }
+});
