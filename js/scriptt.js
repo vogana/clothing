88e109083fc448b4381251f26a2790db4447bb2a
vogana <cctv92@126.com>
2023-08-03 20:30:28 +0800

    clothing

--- js/scriptt.js
+++ js/scriptt.js
@@ -8,9 +8,9 @@ toggleBtn.addEventListener('click', () => {
   // 切换面板的显示状态
   if (panel.style.display === 'none') {
     panel.style.display = 'block'; // 显示面板
-   
+  toggleBtn.style.right = 'calc(150px)'; // 垂直居中 
   } else {
     panel.style.display = 'none'; // 隐藏面板
-    
+ toggleBtn.style.right = 'calc(0px)'; // 垂直居中   
   }
 });
